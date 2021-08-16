8db7c1720de042aea72abf85f4bb722b3be5eb3b
o-gol <olejkai@yandex.ru>
2021-08-16 19:17:41 +0100

    Initial commit

---
+++ src/main/java/ru/olejkai/task_vsr/TaskVsrApplication.java
@@ -0,0 +1,13 @@
+package ru.olejkai.task_vsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskVsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskVsrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/olejkai/task_vsr/TaskVsrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.olejkai.task_vsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskVsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
